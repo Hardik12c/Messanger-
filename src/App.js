5535b81a0f8d6484992835169e324aeddeee7eda
Hardik Garg <93364270+Hardik12c@users.noreply.github.com>
2023-01-01 00:12:58 +0530

    completely functioning added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
-import './App.css';
-import { useState } from 'react';
-import { Button,FormControl,Input,InputLabel} from '@mui/material'
-import Messages from './components/Messages';
+import "./App.css";
+import { useState,useEffect } from "react";
+import { Button, FormControl, Input, InputLabel } from "@mui/material";
+import Messages from "./components/Messages";
 
 function App() {
-  const [input, setinput] = useState('');
+  const [input, setinput] = useState("");
 
-  const [message, setmessage] = useState(['hello']);
+  const [message, setmessage] = useState([
+    { username: "hardik", text: "hello" },
+    { username: "Dharmahendra", text: "Good morning" },
+  ]);
+  const [username, setusername] = useState('');
 
+  useEffect(() => {
+    setusername(prompt('Please Enter your name'));
+  },[])
+  
 
-  const changeinput=(e)=>{
+  const changeinput = (e) => {
     setinput(e.target.value);
-  }
+  };
 
-  const sendmessage=(e)=>{
+  const sendmessage = (e) => {
     e.preventDefault();
-    setmessage([...message,input]);
-    setinput('');
-  }
-  console.log(message);
+    setmessage([...message, {username:username,text:input}]);
+    setinput("");
+  };
   return (
     <div className="App">
       <h1>Hello i am hardik here</h1>
       <FormControl>
         <InputLabel>Enter text Here</InputLabel>
-        <Input type="text" value={input} onChange={changeinput}/>
-        <Button variant='contained' disabled={!input} onClick={sendmessage}>Submit</Button>
+        <Input type="text" value={input} onChange={changeinput} />
+        <Button variant="contained" disabled={!input} onClick={sendmessage}>
+          Submit
+        </Button>
       </FormControl>
-      {
-        message.map((m)=>{
-          return <Messages text={m}/>
-        })
-      }
+      {message.map((m) => {
+        return <Messages message={m} username={username}/>;
+      })}
     </div>
   );
 }
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { CardContent, Typography, Card } from "@mui/material";
+import "../assets/Message.css";
 export default function Messages(props) {
-    return (
-        <div>
-            <h2>{props.text}</h2>
-        </div>
-    )
+    const isuser=props.username===props.message.username;
+    // console.log(isuser);
+  return (
+    <div className={`message ${isuser&&"userown"}`}>
+        <Card className={isuser?"message-user":"message-guest"}>
+        <CardContent>
+            <Typography variant="h5" component="h2">
+            {props.message.username}:{props.message.text}
+            </Typography>
+        </CardContent>
+        </Card>
+    </div>
+  );
 }
