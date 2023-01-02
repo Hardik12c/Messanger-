0bcb8a74ffab6c4eb61bc812e92a9cd351123835
Hardik Garg <93364270+Hardik12c@users.noreply.github.com>
2023-01-03 03:07:42 +0530

    database write operations added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import "./App.css";
+import logo from "./assets/logo.svg"
 import { useState,useEffect } from "react";
-import { Button, FormControl, Input, InputLabel } from "@mui/material";
+import {FormControl, Input, InputLabel } from "@mui/material";
 import Messages from "./components/Messages";
 import db from "./db/firebase";
+import firebase from 'firebase/compat/app';
+import FlipMove from 'react-flip-move';
+import { IconButton } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
 
 function App() {
   const [input, setinput] = useState("");
 
-  const [message, setmessage] = useState([
-    { username: "hardik", text: "hello" },
-    { username: "Dharmahendra", text: "Good morning" },
-  ]);
+  const [messages, setmessages] = useState([]);
   const [username, setusername] = useState('');
+  const sendmessage = (e) => {
+    e.preventDefault();
+    db.collection('Messages').add({
+      text:input,
+      username:username,
+      timestamp:firebase.firestore.FieldValue.serverTimestamp()
+    });
+    setinput("");
+  };
 
   useEffect(() => {
-    db.collection('Messages').onSnapshot(snapshot=>{
-      setmessage(snapshot.docs.map(doc=> doc.data()))
+    db.collection('Messages').orderBy('timestamp','desc').onSnapshot(snapshot=>{
+      setmessages(snapshot.docs.map(doc=> ({id:doc.id,message:doc.data()})))
     })
   }, [])
   
@@ -28,24 +39,26 @@ function App() {
     setinput(e.target.value);
   };
 
-  const sendmessage = (e) => {
-    e.preventDefault();
-    setmessage([...message, {username:username,text:input}]);
-    setinput("");
-  };
   return (
     <div className="App">
-      <h1>Hello i am hardik here</h1>
-      <FormControl>
-        <InputLabel>Enter text Here</InputLabel>
-        <Input type="text" value={input} onChange={changeinput} />
-        <Button variant="contained" disabled={!input} onClick={sendmessage}>
-          Submit
-        </Button>
-      </FormControl>
-      {message.map((m) => {
-        return <Messages message={m} username={username}/>;
-      })}
+      <img className="logo" src={logo} alt="" />
+      <h1>Welocome to I-chat</h1>
+      <h2>Hello {username}</h2>
+      <form className="app__form">
+        <FormControl>
+          <InputLabel>Enter text Here</InputLabel>
+          <Input type="text" value={input} onChange={changeinput} />
+          <IconButton color="primary" variant="contained" disabled={!input}  onClick={sendmessage}>
+            <SendIcon />
+          </IconButton>
+        </FormControl>
+      </form>
+
+      <FlipMove>
+      {messages.map(({id,message}) => (
+        <Messages key={id} message={message} username={username}/>
+        ))}
+      </FlipMove>
     </div>
   );
 }
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React,{ forwardRef } from "react";
 import { CardContent, Typography, Card } from "@mui/material";
 import "../assets/Message.css";
-export default function Messages(props) {
-    const isuser=props.username===props.message.username;
-    // console.log(isuser);
+const Messages=forwardRef((props,ref)=> {
+  const isuser=props.username===props.message.username;
   return (
-    <div className={`message ${isuser&&"userown"}`}>
+    <div ref={ref} className={`message ${isuser&&"userown"}`}>
         <Card className={isuser?"message-user":"message-guest"}>
         <CardContent>
             <Typography variant="h5" component="h2">
-            {props.message.username}:{props.message.text}
+            {!isuser &&`${props.message.username}:`} {props.message.text}
             </Typography>
         </CardContent>
         </Card>
     </div>
   );
-}
+})
+
+export default Messages
